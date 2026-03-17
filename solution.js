a7c41d8ecf31af0728d9456a4181335d6eab5b11
Shem Tafalla/Mark Obrino <tafallaexcequelshem@gmail.com>
2026-03-17 21:34:00 +0800

    Problem5

--- solution.js
+++ solution.js
@@ -1,19 +1,14 @@
-function getTopScorers(playerList) {
-    return playerList
-        .filter(player => player.score > 8)
-        .map(player => player.name)
-        .join(", ");
 }
 
-class Item {
-    #discount = 0.1;
-
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    get finalPrice() {
-        return this.price - (this.price * this.#discount);
+function safeDivide(a, b) {
+    try {
+        if (b === 0) {
+            throw new Error("Cannot divide by zero");
+        }
+        return a / b;
+    } catch (error) {
+        return error.message;
+    } finally {
+        console.log("Operation attempted");
     }
 }
