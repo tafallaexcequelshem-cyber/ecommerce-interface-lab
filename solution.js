c853b0494789c6a8544d6eaca78a4f1afdda277c
Shem Tafalla/Mark Obrino <tafallaexcequelshem@gmail.com>
2026-03-17 21:19:57 +0800

    Problem1

---
+++ solution.js
@@ -0,0 +1,18 @@
+function checkVariable(input) {
+    switch (typeof input) {
+        case "string":
+            return "string";
+        case "number":
+            return "number";
+        case "boolean":
+            return "boolean";
+        case "bigint":
+            return "bigint";
+        case "undefined":
+            return "undefined";
+        case "object":
+            return "object";
+        default:
+            return "unknown";
+    }
+}
